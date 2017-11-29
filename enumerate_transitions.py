4883db6a2ccdf352296a3b6a818f334a32cdd894
Casey Doran <cdoran2011@my.fit.edu>
2017-11-29 03:04:38 -0500

    delete yeager-specific code from traditional tests

--- enumerate_transitions.py
+++
@@ -1,9 +0,0 @@
-from yeager import enumerate_transitions
-
-# parse the transtitions...
-import state_transitions.headerpage
-import state_transitions.contacts
-import state_transitions.login
-
-if __name__ == "__main__":
-    enumerate_transitions()
--- yeager_test.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from yeager import walk
-
-# parse the transtitions...
-import state_transitions.headerpage
-import state_transitions.contacts
-import state_transitions.login
-
-walk(driver=webdriver.Chrome())
